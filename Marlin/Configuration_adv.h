313c4a367f46c543cba591f366b6d28696a6d133
Sander Boer <sanderboer@mauc.nl>
2020-09-21 16:10:22 +0200

    tried and failed to get lin_advance to work time to buy an skr

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -952,6 +952,7 @@
  * vibration and surface artifacts. The algorithm adapts to provide the best possible step smoothing at the
  * lowest stepping frequencies.
  */
+
 #define ADAPTIVE_STEP_SMOOTHING
 
 /**
@@ -1622,7 +1623,7 @@
 // #define LIN_ADVANCE
 #if ENABLED(LIN_ADVANCE)
   //#define EXTRA_LIN_ADVANCE_K // Enable for second linear advance constants
-  #define LIN_ADVANCE_K 0.22    // Unit: mm compression per 1mm/s extruder speed
+  #define LIN_ADVANCE_K 0    // Unit: mm compression per 1mm/s extruder speed
   //#define LA_DEBUG            // If enabled, this will generate debug information output over USB.
   //#define EXPERIMENTAL_SCURVE // Enable this option to permit S-Curve Acceleration
 #endif
@@ -2551,7 +2552,7 @@
    * Enable M122 debugging command for TMC stepper drivers.
    * M122 S0/1 will enable continous reporting.
    */
-  //#define TMC_DEBUG
+ // #define TMC_DEBUG
 
   /**
    * You can set your own advanced settings by filling in predefined functions.
